a79eaa0a1734d1dac96643af35e7c4afb51765f2
Damola Adediran <tylher123@gmail.com>
2022-04-05 13:22:24 +0100

    configure linters for project

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,24 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Leader board',
+    }),
+  ],
+  module: {
+    rules: {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
     },
-    mode: 'development',
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Leader board',
-        }),
-    ],
-    module: {
-        rules:{
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-        },
-    },
-    devServer: {
-        static : './dist'
-    },
-}
+  },
+  devServer: {
+    static: './dist',
+  },
+};
